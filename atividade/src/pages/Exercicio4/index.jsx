cf7a08f6ab4cbe490dd24b6e7e089b223418f9d9
filipelomba <filipelomba@yahoo.com.br>
2024-06-05 22:28:17 -0300

    feat: exercicio 4

--- atividade/src/pages/Exercicio4/index.jsx
+++ atividade/src/pages/Exercicio4/index.jsx
@@ -1,9 +1,26 @@
 import './style.css'
+import { useEffect, useState } from 'react';
+
+const colors = ["", "#ffff00", "#0000ff", "#008000", "#ff0000"]
 
 function Exercicio4() {
-  return (
-    <div>
+  const [position, setPosition] = useState(0)
+  const [cor, setCor] = useState(colors[position])
 
+  const click = () => {
+    if(position === colors.length -1)
+    setPosition(1)
+    else setPosition(position + 1)
+  }
+
+  useEffect(()=>{
+    setCor(colors[position])
+  }, [position])
+
+  return (
+    <div className='Exercicio4' style={{backgroundColor: cor}}>
+      <button className='botao' onClick={click}>Mude a cor!</button>
+      <p className='texto'>{cor}</p>
     </div>
   );
 }
